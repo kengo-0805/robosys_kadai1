27a3e1931b6d1916b25d8ab9cb551435056c2567
Kengo Horii <kengo0805ooo@gmail.com>
2020-12-06 15:14:50 +0900

    modifyd

--- myled.c
+++ myled.c
@@ -27,7 +27,7 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
     	char c;
     	int i = 0;
     	if(copy_from_user(&c,buf,sizeof(char))){
-        return -EFAULT;
+	        return -EFAULT;
 	}
 
 	if(c == '0'){
